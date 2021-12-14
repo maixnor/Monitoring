99be58dffc23e42f6bcf8cd53509cb5172d15194
maixnor <46966993+maixnor@users.noreply.github.com>
2021-12-14 10:08:05 +0100

    configure metrics

--- MonitoringApi/Program.cs
+++ MonitoringApi/Program.cs
@@ -1,3 +1,6 @@
+using App.Metrics.Formatters.Json;
+using App.Metrics.Formatters.Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMetrics();
+builder.Services.AddMetricsTrackingMiddleware();
+builder.Services.AddMetricsEndpoints();
 
 var app = builder.Build();
 
@@ -15,6 +21,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMetricsEndpoint(new MetricsPrometheusProtobufOutputFormatter());
+app.UseMetricsTextEndpoint(new MetricsJsonOutputFormatter());
 
 app.UseHttpsRedirection();
 
